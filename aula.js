3d47c0ade1d3b11676c08765c8e2d22227e0a695
danielpetenon <87392178091@senacrs.edu.br>
2024-11-26 16:54:38 -0300

    Projeto
    Co-Authored-By: Sandro Martins da Costa <sandromartinscosta@gmail.com>

---
+++ aula.js
@@ -0,0 +1,4 @@
+let Dani = "Daniel Petenon Ciarlini";
+Dani.corOlho = "castanhos";
+Dani.idade = 16;
+Dani.Levantar();
---
+++ perguntas.js
@@ -0,0 +1,80 @@
+const Quiz = [ 
+  {"question": "qual o nome do meteoro que matou os dinossauros?",
+  "A": "Chicxulub",
+  "B": "Macau",
+  "C": "Willamette",
+  "D": "hoba",
+  "answer": "A"
+}, 
+  {
+    "question": "onde caiu o meteoro que matou os dinossauros?",
+    "A": "espanha",
+    "B": "india",
+    "C": "russia",
+    "D": "mexico",
+    "answer": "D"
+  }, 
+  {
+    "question": "qual foi o MENOR dinossauro que ja existiu?",
+    "A": "Oculudentavis",
+    "B": "Epidexipteryx hui",
+    "C": "Anchiornis",
+    "D": "Microraptor",
+    "answer": "A"
+  }, 
+  {
+    "question": "qual foi o maior dinossauro que existiu?",
+    "A": "Argentinosaurus huinculensis",
+    "B": "Uberabatitan ribeiroi",
+    "C": "Patagotitan mayorum",
+    "D": "Padillasaurus Leivaensis",
+    "answer": "C"
+  }, 
+  {
+    "question": "qual foi o maior dinossauro aquático do mundo?",
+    "A": "Mosassauros",
+    "B": "honisaurus",
+    "C": "Espinossauro",
+    "D": "Ictiossauros",
+    "answer": "B"
+  }, 
+  {
+    "question": "qual era dos dinossauros que durou mais tempo?",
+    "A": "Cretáceo",
+    "B": "Jurássico",
+    "C": "Triássico",
+    "D": "Mesozoica",
+    "answer": "A"
+  }, 
+  {
+    "question": "apos o impacto do meteoro, qual especies ainda se materam vivas?",
+    "A": "marinhas",
+    "B": "herbivoros",
+    "C": "carnivoros",
+    "D": "aves",
+    "answer": "D"
+  }, 
+  {
+    "question": "qual era o dinossauro mais veloz do mundo?",
+    "A": "velociraptor",
+    "B": "Compsognathus",
+    "C": "Santanaraptor",
+    "D": "Dilofossauro",
+    "answer": "B"
+  },   {
+    "question": "qual foi o maior PREDADOR do mundo?",
+    "A": "Tiranossauro Rex",
+    "B": "Mosassauros",
+    "C": "Espinossauro",
+    "D": "giganotossauro",
+    "answer": "C"
+  }, 
+  {
+    "question": "qual ano foi encontrado o primeiro fossil de dinossauro?",
+    "A": "1785",
+    "B": "1983",
+    "C": "1676",
+    "D": "1854",
+    "answer": "C"
+  }
+]
